a29f0dffa4823f7009ea90c705664652337c6a5e
Anna Kwadrans <annakwadrans@MacBook-Air-de-Anna.local>
2024-10-14 17:35:17 +0200

    correct putnbr

--- ft_put.c
+++ ft_put.c
@@ -6,7 +6,7 @@
 /*   By: akwadran <akwadran@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 20:56:26 by akwadran          #+#    #+#             */
-/*   Updated: 2024/09/28 08:17:24 by akwadran         ###   ########.fr       */
+/*   Updated: 2024/10/14 17:13:56 by akwadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,31 +37,31 @@ void	ft_putendl_fd(char *s, int fd)
 	}
 	write(fd, "\n", 1);
 }
-/*
+
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n >= -2147483648 && n <= 2147483647)
 	{
 		if (n < 0)
 		{
-			write(fd, "-", 1);
+			ft_putchar_fd('-', fd);
 			if (n == -2147483648)
 			{
-				write(fd, "2", 1);
+				ft_putchar_fd('2', fd);
 				n = -147483648;
 			}
 			n = -n;
 		}
 		if (n >= 10)
 		{
-			ft_putnbr_fd((n / 10), 1);
-			ft_putchar_fd(((n % 10) + '0'), 1);
+			ft_putnbr_fd((n / 10), fd);
+			ft_putchar_fd(((n % 10) + '0'), fd);
 		}
 		if (n < 10)
-			ft_putchar_fd((n + '0'), 1);
+			ft_putchar_fd((n + '0'), fd);
 	}
 }
-*/
+
 /*
 int	main(void)
 {
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: akwadran <akwadran@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 20:45:45 by akwadran          #+#    #+#             */
-/*   Updated: 2024/10/12 15:34:12 by akwadran         ###   ########.fr       */
+/*   Updated: 2024/10/14 17:23:53 by akwadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*)); //OK
 void	ft_putchar_fd(char c, int fd); //OK
 void	ft_putstr_fd(char *s, int fd); //OK
 void	ft_putendl_fd(char *s, int fd); //OK
-//void	ft_putnbr_fd(int n, int fd); // corregir
+void	ft_putnbr_fd(int n, int fd); //OK
 
 //t_list *ft_lstnew(void *content);
 //void ft_lstadd_front(t_list **lst, t_list *new);
